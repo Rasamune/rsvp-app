5fa7167f63a2d3b58ebe7edb8a3875e128dfb548
Christopher Jaeger <cjaeger.ld@gmail.com>
2020-11-26 15:18:43 -0500

    Added Comments

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const filterLabel = document.createElement('label');
   const filterCheckbox = document.createElement('input');
 
+  // ---------------
+  // Filter Checkbox
   filterLabel.textContent = "Hide those who haven't responded";
   filterCheckbox.type = 'checkbox';
   div.appendChild(filterLabel);
   div.appendChild(filterCheckbox);
   mainDiv.insertBefore(div, ul);
   
+  // If checkbox is changed (checked)
   filterCheckbox.addEventListener('change', (e) => {
     const isChecked = e.target.checked;
     const lis = ul.children;
     if (isChecked) {
+      // If checked, hide all invitees without the 'responded' class
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
         if (li.className === 'responded') {
@@ -27,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     } else {
+      // If unchecked, show all invitees
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
         li.style.display = '';
@@ -34,32 +39,43 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   
+  // --------------
+  // "Invitee Card" (list object)
   function createLI(text) {
+    // Create a new element with the provided parameters
     function createElement(elementName, property, value) {
       const element = document.createElement(elementName);
       element[property] = value;
       return element;
     }
+    // Append new element to the invitee card (list object)
     function appendToLI(elementName, property, value) {
       const element = createElement(elementName, property, value);
       li.appendChild(element);
       return element;
     }
+    // Create the Invitee Card's main List Element
     const li = document.createElement('li');
+    // Create the Invitee Card's details
     appendToLI('span', 'textContent', text);
-    appendToLI('label', 'textContent', 'Confirmed')
+    appendToLI('label', 'textContent', 'Confirm')
       .appendChild(createElement('input', 'type', 'checkbox'));
     appendToLI('button', 'textContent', 'edit');
     appendToLI('button', 'textContent', 'remove');
 
     return li;
   }
-  
+
+  // ----------------------
+  // Duplicate Text & Alert
+  // Check if the given text is duplicated among the invitee list
   function checkForDuplicate(text) {
     const listNames = ul.children;
     let nameDuplicate = false;
+    // Loop through all invitee lists
     for (let i = 0; i < listNames.length; i++) {
       let listName = listNames[i].firstChild;
+      // Avoid checking any INPUT fields by filtering SPAN elements
       if (listName.tagName === 'SPAN') {
         if ( listName.textContent.toLowerCase() === text.toLowerCase()) {
           nameDuplicate = true;
@@ -70,6 +86,8 @@ document.addEventListener('DOMContentLoaded', () => {
     return nameDuplicate;
   }
 
+  // -------------------
+  // Blank field & Alert
   function fieldIsBlank(text) {
     if (text === '') {
       alert("Field cannot be blank");
@@ -79,8 +97,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // -----------
+  // Form Submit
   form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevent page from reloading on submission
     const text = input.value;
     if (!fieldIsBlank(text)) {
       input.value = '';
@@ -91,6 +111,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   
+  // ---------------------
+  // Confirmation Checkbox
   ul.addEventListener('change', (e) => {
     const checkbox = e.target;
     const checked = checkbox.checked;
@@ -102,7 +124,10 @@ document.addEventListener('DOMContentLoaded', () => {
       listItem.className = '';
     }
   });
-  
+
+  // --------------------
+  // Invitee Card Buttons
+  // Button click listener for EDITING, SAVING & REMOVING Invitees
   ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
       const button = e.target;
@@ -110,9 +135,11 @@ document.addEventListener('DOMContentLoaded', () => {
       const ul = li.parentNode;
       const action = button.textContent;
       const nameActions = {
+        // REMOVE Invitee Card Completely
         remove: () => {
           ul.removeChild(li); 
         },
+        // EDIT Invitee Name
         edit: () => {
           const span = li.firstElementChild;
           const input = document.createElement('input');
@@ -122,6 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
           li.removeChild(span);
           button.textContent = 'save';
         },
+        // SAVE Changes to Invitee Name
         save: () => {
           const input = li.firstElementChild;
           if (!checkForDuplicate(input.value)) {
